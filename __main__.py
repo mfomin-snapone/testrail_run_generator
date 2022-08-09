44ab96dc841c8fc74509b18122f8acf6470aab2f
Mark Fomin <mfomin93@gmail.com>
2022-08-09 11:34:18 -0400

    Added optional include all test cases argument

--- __main__.py
+++ __main__.py
@@ -6,6 +6,7 @@ parser._action_groups.pop()
 parser.add_argument('--project_id', default=None)
 parser.add_argument('--suite_id', default=None)
 parser.add_argument('--testrun_name', default=None)
+parser.add_argument('--include_all_cases', default=None)
 parser.add_argument("--list", nargs="+", default=["a", "b"])
 args = parser.parse_args()
 
@@ -26,7 +27,7 @@ def testRunID():
                                        , suite_id
                                        , test_run_name
                                        , 'Test run created using the TestRail.'  # required.  test run description
-                                       , include_all_cases=False
+                                       , include_all_cases=args.include_all_cases
                                        , case_ids=args.list
                                        # optional.  This is already defaulted to True i only included it here for visibility.  not required for this example
                                        # , milestone_id='Some meaningful milestone'# optional.  use if you want to link this test run to a milestone
